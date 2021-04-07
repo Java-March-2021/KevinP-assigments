9ddb3b6c781b9f414573141d2b5e7209a9ed4b2f
Kevin Pacheco <kpacheco1128@gmail.com>
2021-04-07 01:09:14 -0700

    HelloHuman

---
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/hellohuman/src/main/java/com/kpacheco/hellohuman/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.kpacheco.hellohuman.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index(@RequestParam (value="q", required=false, defaultValue="Human") String name, Model model)
+	{
+		model.addAttribute("name", name);
+		return "index.jsp";
+	}
+			
+
+}
